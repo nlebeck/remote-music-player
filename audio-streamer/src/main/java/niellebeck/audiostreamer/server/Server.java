ba2e87eabba23d0fdf98832f86bad5e3af292f9b
Niel Lebeck <niel.lebeck@gmail.com>
2019-05-04 11:23:55 -0700

    Add method to print info about available mixers/lines

--- audio-streamer/src/main/java/niellebeck/audiostreamer/server/Server.java
+++ audio-streamer/src/main/java/niellebeck/audiostreamer/server/Server.java
@@ -1,7 +1,30 @@
 package niellebeck.audiostreamer.server;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Line;
+import javax.sound.sampled.Mixer;
+
 public class Server {
 	public static void main(String[] args) {
-		System.out.println("TODO: implement me.");
+		Server server = new Server();
+		server.printMixerInfo();
+	}
+	
+	private void printMixerInfo() {
+		Mixer.Info[] infos = AudioSystem.getMixerInfo();
+		for (Mixer.Info info : infos) {
+			System.out.println(info.getName());
+			Mixer mixer = AudioSystem.getMixer(info);
+			Line.Info[] sourceLineInfos = mixer.getSourceLineInfo();
+			System.out.println("  Source lines:");
+			for (Line.Info lineInfo : sourceLineInfos) {
+				System.out.println("    " + lineInfo.getLineClass());
+			}
+			System.out.println("  Target lines:");
+			Line.Info[] targetLineInfos = mixer.getTargetLineInfo();
+			for (Line.Info lineInfo : targetLineInfos) {
+				System.out.println("    " + lineInfo.getLineClass());
+			}
+		}
 	}
 }
